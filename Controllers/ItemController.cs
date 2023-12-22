975e66418223c446f01380efeeac65bf444bc86e
Yuri Mateus Lira da Costa <y.costa@academico.ifrn.edu.br>
2023-12-22 09:13:55 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using AlugueldeTemas.Models; // Substitua "AlugueldeTemas" pelo namespace correto do seu projeto
+using AlugueldeTemas.Services;
+
+namespace AlugueldeTemas.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ItemService _itemService;
+
+        public ItemController()
+        {
+            _itemService = new ItemService(); // Certifique-se de ajustar isso de acordo com a sua implementação de serviço
+        }
+
+        public ActionResult Index()
+        {
+            List<ItemTema> itens = _itemService.ListarItens();
+            return View(itens);
+        }
+
+        public ActionResult Details(int id)
+        {
+            ItemTema item = _itemService.ObterItemPorId(id);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ItemTema itemTema)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _itemService.GravarItem(itemTema);
+                    return RedirectToAction("Index");
+                }
+
+                return View(itemTema);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ItemTema item = _itemService.ObterItemPorId(id);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ItemTema itemTema)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _itemService.AtualizarItem(itemTema);
+                    return RedirectToAction("Index");
+                }
+
+                return View(itemTema);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ItemTema item = _itemService.ObterItemPorId(id);
+
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _itemService.ExcluirItem(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
